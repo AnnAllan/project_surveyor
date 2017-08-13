8bab75c597e4f7a48676cd0061823af54a5ced41
Ann Allan <ann.cristina.allan@gmail.com>
2017-08-12 22:19:41 -0400

    Build surveys

--- surveyor/app/controllers/surveys_controller.rb
+++ surveyor/app/controllers/surveys_controller.rb
@@ -1,2 +1,27 @@
 class SurveysController < ApplicationController
+
+  def index
+    @surveys = Survey.all
+  end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def create
+    @survey = Survey.new(safe_survey_params)
+    if @survey.save
+      flash[:success] = "Survey created."
+      redirect_to root_path
+    else
+      flash.now[:error] = "Survey could not be created."
+      render :new
+    end
+  end
+
+
+  private
+  def safe_survey_params
+    params.require(:survey).permit(:title, :description, :responses)
+  end
 end
--- surveyor/db/migrate/20170811215321_create_surveys.rb
+++ surveyor/db/migrate/20170811215321_create_surveys.rb
@@ -1,6 +1,9 @@
 class CreateSurveys < ActiveRecord::Migration[5.0]
   def change
     create_table :surveys do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :responses
 
       t.timestamps
     end
